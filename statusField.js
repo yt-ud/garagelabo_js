8493402e7df4bb577f7c3e9157eeb1a6ee52aefb
yutaueda <yufrom2018@gmail.com>
2019-05-13 05:39:14 +0900

    add comment by no-approve

--- statusField.js
+++ statusField.js
@@ -3,6 +3,9 @@ var ardpp_value_conversion = ['app.record.detail.process.proceed'];
 kintone.events.on(ardpp_value_conversion, function(event) {
 
     var record = event['record'];
+    var appId = kintone.app.getId();
+    var recordId = kintone.app.record.getId();
+
     var nStatus = event.nextStatus.value
 
     if (nStatus === '承認待ち') {
@@ -12,7 +15,21 @@ kintone.events.on(ardpp_value_conversion, function(event) {
         record['status']['value'] = '完了'
         return event;
     } else if (nStatus === 'さしもどし') {
-        var reason = window.prompt("差し戻し理由", "理由を記入してください")
+        var reason = prompt('差し戻し理由', '');
+        var body = {
+            "app": appId,
+            "record": recordId,
+            "comment": {
+                "text": '【差し戻し理由】' + '\n' + reason
+            }
+        }
+        kintone.api(kintone.api.url('/k/v1/record/comment', true), 'POST', body, function(resp) {
+            //success
+            console.log(resp);
+        }, function(error) {
+            //error
+            console.log(error);
+        });
         record['status']['value'] = '未申請'
         return event;
     }
